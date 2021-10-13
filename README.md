551dbfa2585c4174eda631503c446f77d77d93cf
novrinjr <novrinjr@gmail.com>
2021-10-13 10:48:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Belajar-Python-Dasar
+ Disini saya akan belajar Python Dari awal
---
+++ operasi_matematika.py
@@ -0,0 +1,9 @@
+a,b = 30,25
+pengurangan = a-b
+print ("Hasil pengurangan : ", pengurangan)
+penjumlahan = a+b
+print ("Hasil penjumlahan: ", penjumlahan)
+pembagian = a/b
+print ("Hasil pembagian: ", pembagian)
+perkalian = a*b
+print ("Hasil perkalian: ", perkalian)
