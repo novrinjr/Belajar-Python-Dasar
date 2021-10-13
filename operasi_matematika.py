6a6cd1f872c1624165cb5df7d80ddbda16b2bff3
novrinjr <novrinjr@gmail.com>
2021-10-13 10:56:38 +0700

    Update operasi_matematika.py

--- operasi_matematika.py
+++ operasi_matematika.py
@@ -1,4 +1,7 @@
+# Disini kita akan belajar cara menghitung angka menggunakan operasi matematika yaitu +,-,*,/ 
+# untuk operator "**" menunjukan operasi perpangkatan
 a,b = 30,25
+c,d = 2,5
 pengurangan = a-b
 print ("Hasil pengurangan : ", pengurangan)
 penjumlahan = a+b
@@ -7,3 +10,5 @@ pembagian = a/b
 print ("Hasil pembagian: ", pembagian)
 perkalian = a*b
 print ("Hasil perkalian: ", perkalian)
+pangkat = d ** c
+print ("Hasil pangkat: ",pangkat)
